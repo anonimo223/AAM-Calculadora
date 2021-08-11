97a235216435b9e1e8f4997f080c507f1d4e5cac
anonimo223 <anguielugo44@gmail.com>
2021-08-11 13:03:20 -0500

    Daniela tapiero: Se hizo la estructura del desarrollo de una calculadora

---
+++ git.py
@@ -0,0 +1,23 @@
+class trabajo:
+
+    def __init__ (self, numero1, numero2):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+    def funcion_sumar(self):
+        return self.numero1 + self.numero2
+
+    def funcion_restar(self):
+        return self.numero1 - self.numero2
+
+    def funcion_multiplicar(self):
+        return self.numero1 * self.numero2
+
+    def funcion_dividir(self):
+        return self.numero1 / self.numero2
+
+trabajo_objeto = trabajo(int(input("Numero 1: ")), int(input("Numero 2: ")))
+print(f"El resultado de la suma es: {trabajo_objeto.funcion_sumar()}")
+print(f"El resultado de la resta es: {trabajo_objeto.funcion_restar()}")
+print(f"El resultado de la multiplicacion es: {trabajo_objeto.funcion_multiplicar()}")
+print(f"El resultado de la division es: {trabajo_objeto.funcion_dividir()}")
